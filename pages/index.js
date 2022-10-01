1c2cb00756278ceb7ec2a6db3fe1aa4f5f2c9a1d
Mathieu De Meue <mathieu@demeue.be>
2022-10-01 09:54:01 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import { Fragment } from 'react'
+
+import Head from 'next/head'
+
+const data = require('../data.json')
+
+export default function Home() {
+  const KOFFIETJESBEDRAG = 5.00;
+
+  const calculateCoffeeSavingsDays = () => {
+    const day_start = data.startday;
+    const moment = new Date();
+    const days_passed = ((moment - day_start) / (1000 * 60 * 60 * 24)).toFixed(2);
+    return parseFloat(days_passed);
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Coffee Savings Tracker</title>
+        <meta name="description" content="My personal coffee savings tracker." />
+      </Head>
+
+      <main className="container pt-5 coffee">
+        <div className="row">
+          <div className="col-12">
+            <h1>Coffee Savings Tracker!</h1>
+            <h2>Sinds {data.startdagsstring} zijn er {calculateCoffeeSavingsDays()} dagen voorbij! <br/> Dit bespaarde mij voorlopig {calculateCoffeeSavingsDays() * KOFFIETJESBEDRAG} euro!</h2>
+          </div>
+        </div>
+      </main>
+
+      <footer className="container py-3">
+        <div className="row">
+          <div className="col-12">
+            <p className="m-0 p-0">Made with ❤️ by Smashing Impact!</p>
+          </div>
+        </div>
+      </footer>
+  </Fragment>
+  )
+}
